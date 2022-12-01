111d5b503d0179cc4cdaf21f4a8a1dc66c0cf883
Krista Mätäsniemi <krista.matasniemi@tuni.fi>
2022-12-01 10:36:23 +0200

    Add unit tests of ceil function

---
+++ test/ceil.test.js
@@ -0,0 +1,25 @@
+import chai from "chai"
+import ceil from "../src/ceil.js"
+const expect = chai.expect
+
+describe("Ceil", () => {
+    it("Positiivinen kolmen desimaalin numero pyöristetään ylöspäin kahden desimaalin tarkkuuteen", () =>{
+        expect(ceil(5.002, 2)).to.equal(5.01)
+    });
+   
+    it("Pyöristetään merkkijono-tyyppistä numeroa", () =>{
+       expect(function() {ceil(600, 2)}).to.throw(Error)
+    });
+   
+    it("Pyöristetään null-arvo", () =>{
+       expect(function() {ceil(null, 2)}).to.throw(Error)
+    });
+   
+    it("Pyöristämisen ilmoitettu tarkkuus on null-arvoinen", () =>{
+       expect(ceil(5.002, null)).to.equal(6)
+   });
+   
+   it("Pyöristetään negatiivinen luku", () =>{
+       expect(ceil(-15.38, 1)).to.equal(-15.3)
+   });
+   })
